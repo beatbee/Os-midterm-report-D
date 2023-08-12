9c88098af764eb19c108094383feb0cf399e58e6
belleqss <bellbi123@gmail.com>
2023-08-12 23:14:18 +0700

    update code

---
+++ timing.c
@@ -0,0 +1,16 @@
+/*
+    Task	: timing
+    Author	: Belle
+    School	: Kasetsart University
+    Language	: C
+    Created	: 12 August 2023 [18:27]
+    Algo	: 
+    Status	: 
+*/
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+int main(){
+   
+}
