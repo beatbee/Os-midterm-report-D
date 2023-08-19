13661d09e792025043536887d345a467134a788e
belleqss <bellbi123@gmail.com>
2023-08-19 21:43:58 +0700

    add code

--- timing.c
+++ timing.c
@@ -12,5 +12,24 @@
 #include<stdlib.h>
 
 int main(){
-   
+     int n=100;
+    clock_t start,end;
+    double time;
+    start = clock();
+    for(int i=0;i<n;i++){
+        printf("HELLO WORLD\n");
+    }
+    end = clock();
+    time = (double)(end-start)/CLOCKS_PER_SEC;
+    printf("Time with I/O = %lf\n",time);
+    printf("\n");
+
+    start = clock();
+    for(int i=0;i<n;i++){
+        int x = 0;
+    }
+    end = clock();
+    time = (double)(end-start)/CLOCKS_PER_SEC;
+    printf("Time without I/O= %lf\n",time); 
+    return 0;
 }
